5537c2c031a04cb41e28fdebc10427eccc4843c7
Augustine <smaji08@gmailcom>
2020-01-10 16:29:36 -0500

    done with the comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,13 @@ var tempUnits = "imperial";
 
 $(document).ready(function(){
     
-    var searchCity;
-    var queryURL;
-    var cityLat;
-    var cityLon;
-    var cityName;
-    var cityID;
     var cities = []; 
     var listDiv = $(".cities");
-    var liTag;
+    
     $(".currentcity").hide();
     $(".yourlocation").hide();
+    
+    //to get the current location --- BONUS point ----
     function findme(){
         if (!navigator.geolocation) {
             $(".yourlocation").show();
@@ -38,9 +34,10 @@ $(document).ready(function(){
     
     findme();
     
+    //locastorage handling 
     if(localStorage["cities"]){
         cities = JSON.parse(localStorage["cities"]);
-
+        //if localsorage not empty rendering the cities from the cities array
         cities.forEach(element => {
             liTag = $("<button>").html("<b>" + element + "</b>");
             liTag.attr("data-city",element);
@@ -53,17 +50,24 @@ $(document).ready(function(){
         console.log("no cities");
     }
 
-
+    //on clicking each city button the weather of the corresponding city is displayed
     $(document).on("click",".citybutton",function(){
         cityName = $(this)[0].dataset.city;
         rendercity(cityName);
     });
 
+    //on clicking clear city button, it clears the localstorage and cities array and the rendering Div
+    $(document).on("click",".clearcities", function(){
+        localStorage.clear();
+        listDiv.empty();
+        cities = [];
+        $(".currentcity").hide();
+        $(".fivedays").hide();
+    });
 
-    $("#searchCity").on("click",function(event){
+    //adding searched cities to the list as buttons and adding that to the localstorage as well
+    $("#searchCity").on("click",function(){
 
-        event.preventDefault();
-        
         var inputSearchCity = $(".addcity").val().trim().toLowerCase();
         if (inputSearchCity === ""){
             return;
@@ -72,10 +76,12 @@ $(document).ready(function(){
         inputSearchCity = inputSearchCity.split(" ");
         searchCity = "";
         
+        //Capitalizing the first letters of cities' each word
         inputSearchCity.forEach(element => {
             searchCity +=   " " + element.charAt(0).toUpperCase() + element.slice(1);
         });
         
+        //prevents to add an already existing citi to the list as well as to localstorage
         if (cities.includes(searchCity)){
             $(".addcity").val("");
             return;
@@ -83,6 +89,7 @@ $(document).ready(function(){
         cities.push(searchCity);
         localStorage.setItem("cities", JSON.stringify(cities));
 
+        //displaying the searched citi names on the left pane
         liTag = $("<button>").html("<b>" + searchCity + "</b>");
         liTag.attr("data-city",searchCity);
         liTag.addClass("list-group-item list-group-item-action list-group-item-primary citybutton");
@@ -92,6 +99,7 @@ $(document).ready(function(){
 
     });    
     
+    //Main Ajax call -- displaying the current selected citi's weather
     function rendercity(searchCity){
         queryURL = baseURL + "weather?q=" + searchCity +"&units=" + tempUnits+ "&" + apiKey;
         $(".currentcity").show();
@@ -100,10 +108,15 @@ $(document).ready(function(){
         
         $.ajax({
             url: queryURL,
-            method: "GET"
-            })
+            method: "GET",
+            statusCode: {
+                404: function() {
+                    $(".currentcity").hide();
+                    alert("Sorry! City" + searchCity+" not found");
+                }
+            }
+        })
         .then(function(response){
-
             console.log(response);
             cityName = response.name;
             cityID = response.id;
@@ -112,17 +125,19 @@ $(document).ready(function(){
             cityLon = response.coord.lon;
             var icon = response.weather[0].icon;
             
+            //displayijg current time along with cityname and countryname
             var h3 = $("<h3>");
             $(".currentcity").append($("<h4>").text(moment().format('ll')));
             h3.append($("<span>").text(cityName + ", " + countryName));
-                       
+            
+            //images for the icons and if sunny clear sky (code = 01d), one custom sunny icon added
             if (icon == "01d"){
                 h3.append($("<span>").append($("<img>").attr({src:"assets/images/sunny.png",height: "15%", width:"20%"})));
             }
             else{
                 h3.append($("<span>").append($("<img>").attr("src","https://openweathermap.org/img/wn/" + icon +"@2x.png")));
             }
-            
+
             var spanTemp = $("<span>");
             spanTemp.html(Math.round(response.main.temp) + " \u2109");
             spanTemp.addClass("spantemp");
@@ -132,11 +147,11 @@ $(document).ready(function(){
             $(".currentcity").append($("<p>").html("<b>Wind Speed : " + response.wind.speed + " MPH</b>"));
             
             getuvindex(cityLat,cityLon);
-            // $(".currentcityrow").after($("<h4>").html("<b>5-Day Forecast</b>"));
             getforecast(cityID);
         });
     }
 
+    //Your location is displayed on a div passing the lat and long retrieved using Geolocation to the ajax call
     function iamhere(latitude,longitude){
         queryURL = baseURL + "weather?lat=" + latitude + "&lon=" +longitude + "&units=" + tempUnits+ "&" + apiKey;
 
@@ -153,8 +168,8 @@ $(document).ready(function(){
         }); 
     }
 
+    //ajax call for the UVIndex element
     function getuvindex(lat,lon){
-                
         queryURL = baseURL + "uvi?"+ apiKey + "&lat=" + lat + "&lon=" +lon;
         
         $.ajax({
@@ -172,40 +187,39 @@ $(document).ready(function(){
 
     }
 
+    //ajax call for the 5-day forecast
     function getforecast(cityID){
+        $(".fivedays").show();
         queryURL = baseURL + "forecast?id=" + cityID + "&units=" + tempUnits + "&" + apiKey;
-
+        
         $.ajax({
             url: queryURL,
             method: "GET"
         })
         .then(function(response){
-
-            // $(".currentcityrow").after($("<h4>").html("<b>5-Day Forecast</b>"));
             
+            //function to return the weekday of the date
             function dayOfWeekAsString(dayIndex) {
                 return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayIndex];
             }            
             
-            for (var i=0;i<40;i+=8){
+            //the for loop to get 1 record each from every day for the next 5 days
+            for (var i=7;i<40;i+=8){
                 var rowCont = $(".fivedays");
                 var colDiv = $("<div>").attr("class","col-sm-2 eachday");
 
-                if (i%2 == 0){
-                    colDiv.attr("style","background-color:rgb(9, 146, 135)");
-                }
-            
                 var myDate = new Date(response.list[i].dt_txt);
-                var sysdate = new Date();
-
-                if (myDate.getDate() === sysdate.getDate()){
-                    continue;
-                }
-                
-                var dateNew = (myDate.getMonth()+1)+"/"+myDate.getDate()+"/"+myDate.getFullYear();
                 var icon = response.list[i].weather[0].icon;
+                
+                //date formatting
+                var dateNew = (myDate.getMonth()+1)+"/"+myDate.getDate()+"/"+myDate.getFullYear();
                 var day = dayOfWeekAsString(myDate.getDay());
                 day = day.slice(0,3).toUpperCase();
+
+                //just rendering different shades for alternative divs
+                if ((myDate.getDate())%2 == 0){
+                    colDiv.attr("style","background-color:rgb(8, 165, 152)");
+                }
 
                 var pDate = $("<h5>").text(dateNew);
                 var pDay = $("<p>").html("<b>" + day +"</b>");
